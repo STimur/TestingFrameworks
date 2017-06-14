546ea567b8d6745691d0d3812bdc2716768f1671
tsalakhe <Timur.Salakhetdinov@t-systems.ru>
2017-06-14 16:40:01 +0300

    add comment for presentation

--- src/main/java/TestingFrameworks/PowerMock/OrderRepository.java
+++ src/main/java/TestingFrameworks/PowerMock/OrderRepository.java
@@ -6,5 +6,6 @@ import TestingFrameworks.Model.Order;
  * Created by Timur on 14-Jun-17.
  */
 public interface OrderRepository {
+    // Some very computation intensive db load
     Order getOrderById(int id);
 }
