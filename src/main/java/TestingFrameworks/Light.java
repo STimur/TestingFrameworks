a4f60137d31cf2932e8471c6fef9178fc99009e4
tsalakhe <Timur.Salakhetdinov@t-systems.ru>
2017-06-14 09:35:58 +0300

    Initial commit

---
+++ src/main/java/TestingFrameworks/Light.java
@@ -0,0 +1,17 @@
+package TestingFrameworks;
+
+public class Light {
+    private boolean on;
+
+    public boolean isOn() {
+        return on;
+    }
+
+    public void switchOn() {
+        on = true;
+    }
+
+    public void switchOff() {
+
+    }
+}
---
+++ src/test/java/TestingFrameworks/LightTest.java
@@ -0,0 +1,22 @@
+package TestingFrameworks;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class LightTest {
+    @Test
+    public void whenSwitchOn_ThenIsOnReturnTrue() throws Exception {
+        Light light = new Light();
+        light.switchOn();
+        assertEquals(true, light.isOn());
+    }
+
+    @Test
+    public void whenSwitchOffAfterSwitchOn_ThenIsOnReturnFalse() throws Exception {
+        Light light = new Light();
+        light.switchOn();
+        light.switchOff();
+        assertEquals(false, light.isOn());
+    }
+}
